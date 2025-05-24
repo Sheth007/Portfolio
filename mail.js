79be9be074d8195d6d6c78d95d19a4f3696017c9
Sheth Uday <113230518+Sheth007@users.noreply.github.com>
2025-05-24 22:28:07 +0530

    updated

--- mail.js
+++ mail.js
@@ -1,10 +1,12 @@
-const Mail_submit_button = document.getElementById('mailredirection');
+const Mail_submit_button = document.getElementById('copymailid');
 
 Mail_submit_button.addEventListener('click', function() {
-    const email = "shethuday505@gmail.com";
-    const subject = encodeURIComponent(document.getElementById('about').value);
-    const body = encodeURIComponent(document.getElementById('message').value);
-
-    const gmailURL = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`;
-    window.location.href = gmailURL;
+    const text = "shethuday505@gmail.com";
+    navigator.clipboard.writeText(text).then(() => {
+        const notify = document.getElementById('notify');
+        notify.style.display = 'block';
+        setTimeout(() => {
+          notify.style.display = 'none';
+        }, 5000);
+      });
 });
