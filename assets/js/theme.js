cb8c9fe61243da06f455aa996733a73e4563c343
Sheth Uday <113230518+Sheth007@users.noreply.github.com>
2026-09-22 22:13:04 +0530

    Updated theme toggle to use same theme everywhere, updated the gap issues in hero desc, also keep the theme toggle at the same place in mobile view

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,13 +1,28 @@
 const schemeBtn = document.getElementById("schemeBtn");
+const savedScheme = localStorage.getItem("scheme");
+
+// Restore saved theme
+if (savedScheme === "midnight") {
+  document.body.classList.replace("scheme-daylight", "scheme-midnight");
+  schemeBtn.textContent = "🌕";
+} else {
+  document.body.classList.replace("scheme-midnight", "scheme-daylight");
+  schemeBtn.textContent = "🌑";
+}
 
 schemeBtn.addEventListener("click", () => {
   const body = document.body;
+
   if (body.classList.contains("scheme-midnight")) {
     body.classList.replace("scheme-midnight", "scheme-daylight");
     schemeBtn.textContent = "🌑";
+
+    localStorage.setItem("scheme", "daylight");
   } else {
     body.classList.replace("scheme-daylight", "scheme-midnight");
     schemeBtn.textContent = "🌕";
+
+    localStorage.setItem("scheme", "midnight");
   }
 });
 
